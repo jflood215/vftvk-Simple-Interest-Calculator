e0dfe6d2fe17639427043200a6bd0920819ea681
John Flood <jflood215@gmail.com>
2021-05-22 15:54:26 -0400

    updated from simple to compound interest

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+// Calculate interest, run form validation, update innerhtml to display results
+function compute() {
+
+let principal = document.getElementById("principal").value;
+let rate = document.getElementById("rate").value;
+let years = document.getElementById("years").value;
+let futureYear = new Date().getFullYear() + parseInt(years);
+
+// let interest = principal * years * rate / 100;
+
+let interest = principal * (1 + rate / 100) ** years - principal
     
-}
-        
+let text = `If you deposit <mark>$${principal}</mark> 
+    at an interest rate of <mark>${rate}%</mark>, 
+    you will receive <mark>$${interest.toFixed(2)}</mark> in interest
+    in the year <mark>${futureYear}.</mark>`;
+
+   document.getElementById('result').innerHTML = text;
+
+   // Validate amount is > 0
+   function validatePrincipal() { 
+    if (principal == '' || principal <= 0){
+        alert('Please enter a positive number');
+}};
+validatePrincipal()
+};
+
+// Display value of slider
+function rangeSlider() {
+    let displayRate = document.getElementById("rate").value;
+   document.getElementById("displayRate").innerHTML = `${displayRate}%`;
+};
